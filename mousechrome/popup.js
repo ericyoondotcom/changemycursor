8ed5d6a9a300cd3ea615828dfb5b89b939ad7546
Eric Yoon <eric@epsilon3.io>
2022-05-12 16:30:25 -0700

    Custom images working

--- mousechrome/popup.js
+++ mousechrome/popup.js
@@ -1,5 +1,14 @@
 
-window.onload = generateElements;
+window.onload = () => {
+  generateElements();
+  registerCallbacks();
+};
+
+const defaultCursors = [
+  "img/among_us.png",
+  "img/cat.png",
+  "img/saber.png",
+];
 
 function changeCursor(cursor) {
   const elems = document.body.getElementsByTagName("*");
@@ -9,34 +18,94 @@ function changeCursor(cursor) {
 }
 
 function generateElements(){
-  console.log("Generating elements")
-  const defaultCursors = [
-    "img/among_us.png",
-    "img/cat.png",
-    "img/saber.png",
-  ].map(i => chrome.runtime.getURL(i));
+  chrome.storage.sync.get({
+    cursors: []
+  }, (data) => {
+    const cursors = defaultCursors
+      .map(i => chrome.runtime.getURL(i))
+      .concat(data.cursors);
 
-  const cursors = defaultCursors;
-  
-  for(let cursor of cursors) {
-    const img = document.createElement("img");
-    img.src = cursor;
-  
-    img.onclick = () => {
-      
-      chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        var currTab = tabs[0];
-        if (!currTab) return;
-        chrome.scripting.executeScript({
-          target: {
-            tabId: currTab.id
-          },
-          func: changeCursor,
-          args: [cursor]
-        });
-      });      
+    const grid = document.getElementById("grid");
+    while(grid.firstChild) grid.removeChild(grid.firstChild);
+
+    for(let cursor of cursors) {
+      const img = document.createElement("img");
+      img.src = cursor;
+    
+      img.onclick = () => {
+        
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+          var currTab = tabs[0];
+          if (!currTab) return;
+          chrome.scripting.executeScript({
+            target: {
+              tabId: currTab.id
+            },
+            func: changeCursor,
+            args: [cursor]
+          });
+        });      
+      }
+      grid.appendChild(img);
     }
-  
-    document.getElementById("grid").appendChild(img);
+  });
+}
+
+function registerCallbacks() {
+  document.getElementById("submit").onclick = onSubmitClicked;
+}
+
+function onSubmitClicked(){
+  const input = document.getElementById("url").value;
+  if(!isValidHttpUrl(input)) {
+    alert("Image URL is not valid! Please enter the URL to an acceptable image type.");
+    return;
   }
+  if(!isValidImage(input)) {
+    alert("The URL specified is not an image. Acceptable types: jpg, gif, png");
+    return;
+  }
+  getImageWidthHeight(input).then((width, height) => {
+    if(width > 32 || height > 32) {
+      alert("The image specified must be smaller than 32x32 pixels.");
+      return;
+    }
+
+    chrome.storage.sync.get({
+      cursors: []
+    }, (data) => {
+      data.cursors.push(input);
+      chrome.storage.sync.set({
+        cursors: data.cursors
+      }, () => {
+        generateElements();
+      })
+    });
+  });
+}
+
+function getImageWidthHeight(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener("load", function() {
+      resolve(this.naturalWidth, this.naturalHeight);
+    });
+    img.src = url;
+  });
+}
+
+function isValidHttpUrl(string) {
+  let url;
+  
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;  
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+function isValidImage(url) {
+  return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
 }
